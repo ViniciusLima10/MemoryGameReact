a138e1995c70c86d308314a56492765a085d1857
Vinicius Lima <viniciusalima1007@gmail.com>
2021-03-25 07:59:39 -0300

    bugfix

--- src/game/game.js
+++ src/game/game.js
@@ -60,7 +60,7 @@ let game = {
     },
 
     checkGameOver(){
-        return this.cards.filter(card=>!card.flipped).length == 0;
+        return this.cards.filter(card=>!card.flipped).length === 0;
     },
 
     
